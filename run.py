26e69055019d02af540fe1a32c0541172746585e
DoubleARon96 <>
2023-12-19 21:30:15 +0000

    feat:i have added a for loop so a grid of 10x10 will be made

--- run.py
+++ run.py
@@ -6,39 +6,24 @@ scores = {"computer":0, "player":0}
 
 
 # Global variables for grid
-grid = [[]]
-
-grid_size = 10
-
-num_of_ships = 8
-
-bullets_left = 50
-
-game_over = False
-
-num_of_ships_sunk = 0
-
-ship_positions = [[]]
-
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 class Playground:
     """
     the main playground class set and sizes set also sets the amount of ships per playground 
     and sets out the playground
     """
 class Grid:
-    def print_grid:
-        ocean_grid = [[i for i in range(j, j + 5)] for j in range(0, 25, 5)]
+    def draw_grid():
+        grid = [["O" for _ in range(10)]for _ in range(10)]
         for row in grid:
-            for element in row:
-                print(element, end="")
-            print()
-class computer_choice():
+            print(" ".join(row))
+            return grid
+
+#class computer_choice():
     """
     random number choice for the grid 
     """
-class Ships():
-    def __init__(self)
-class Player():
-class User_input():
-class End_game
+#class Ships():
+    #def __init__(self)
+#class Player():
+#class User_input():
+#class End_game
