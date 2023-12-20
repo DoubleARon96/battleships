4125b8bdbeaeadb4ca4b5c2d30355362a698a33f
DoubleARon96 <>
2023-12-20 18:54:23 +0000

    feat:i have added a class that prints out the grid and started to get the player name message to print and make it so you have to use the input before the grid prints

--- run.py
+++ run.py
@@ -3,25 +3,67 @@ import time
 
 # computer and player scores set to 0 
 scores = {"computer":0, "player":0}
-
-
 # Global variables for grid
-class Playground:
+player_grid_rows = 5
+player_grid_column = 5
+cpu_grid_rows = 5
+cpu_grid_column = 5
+#global name variable 
+name = ()
+
+class Player_Input:
+    def __init__(self, name, scores):
+        self.name = name
+        self.scores = scores
+
+
+    def get_player_name(self):
+        self.name = input("Enter Name Here")
+        print(f"Output:Welcome to the battle {self.name}")
+        
+        return self.name
+    
+    def check_player_name(self):
+        if():
+            name == ""
+            print("please Enter Name")
+        else:
+            return self.name
+
+
+
+   
+
+   # def __init__(self.name)
+    #    name = player_name
+
+
+print("test1")
+class grid_drawing():
     """
-    the main playground class set and sizes set also sets the amount of ships per playground 
-    and sets out the playground
+    this class draws out the grid
     """
-class Grid:
-    def draw_grid():
-        grid = [["O" for _ in range(10)]for _ in range(10)]
-        for row in grid:
-            print(" ".join(row))
-            return grid
+    for i in range(player_grid_rows):
+        
+        for j in range(player_grid_column):
+            
+            print("O ", end="")
+        
+        print()
+    print("__________ ")
+    for i in range(player_grid_rows):
+        
+        for j in range(player_grid_column):
+            
+            print("O ", end="")
+        
+        print()
+
 
 #class computer_choice():
-    """
-    random number choice for the grid 
-    """
+    #"""
+   # random number choice for the grid 
+   # """
 #class Ships():
     #def __init__(self)
 #class Player():
